94942c7a8ce601a95b604f88adde07b2473a5e88
ddobagi <freshcom@snu.ac.kr>
2025-04-19 14:02:50 +0900

    기본 포트 수정

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { exec } = require('child_process');
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
