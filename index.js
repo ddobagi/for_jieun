6501dc4c965bc40ee28bc325f018d9df20b25d01
ddobagi <freshcom@snu.ac.kr>
2025-04-19 16:21:22 +0900

    원래 포트로 복귀

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { exec } = require('child_process');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 
